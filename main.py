c3ac8b585660406a95ffa37643807105f0070805
PrajwalVandana <72575914+PrajwalVandana@users.noreply.github.com>
2021-11-26 00:31:34 -0800

    deleted pcb files and some changes to main.py

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ PRAJWAL = True
 
 SDA = machine.Pin(0)
 SCL = machine.Pin(1)
-START_STOP_BUTTON = machine.Pin(13)
+START_STOP_BUTTON = machine.Pin(19)
 # CALIB_BUTTON = machine.Pin(14)
-STATUS_LED = machine.Pin(15, machine.Pin.OUT)
+STATUS_LED = machine.Pin(20, machine.Pin.OUT)
 STATUS_LED.value(1)  # test
+DEBUG_LED = machine.Pin(25, machine.Pin.OUT)
 utime.sleep(1)  # test
 STATUS_LED.value(0)
 # ----------------------------- #
@@ -39,33 +40,44 @@ else:
 
 
 i2c = machine.I2C(0, scl=SCL, sda=SDA)
-print(i2c.scan())
+print(i2c.scan())  # DEBUG
 
 _ = mpu9250.MPU9250(i2c)  # opens bypass to access AK8963
 
 mpu6500 = MPU6500(i2c)
 
-if os.path.exists('./magno_offset.txt'):
+try:
     with open('./magno_offset.txt', 'r') as f:
         magno = AK8963(i2c, offset=tuple(map(float, f.read().split())), scale=MAGNO_SCALE)
-else:
+except OSError:
     magno = AK8963(i2c)
 
 sensor = mpu9250.MPU9250(i2c, ak8963=magno, mpu6500=mpu6500)
 
+# serialport = machine.UART(1, 115200)
+# serialport.readline()
+
+# sys.stdout.write('test\n') # DEBUG
 print(START_STOP_BUTTON.value())  # DEBUG
 while True:
     # not logging, checking if connected to app
     while START_STOP_BUTTON.value() != 1:
+        DEBUG_LED.value(1)
+        utime.sleep(0.5)
+        DEBUG_LED.value(0)
         # NOTE: inefficient, consider porting to select.poll()
-        while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        if select.select([sys.stdin], [], [], 0)[0]:
             msg = sys.stdin.readline().rstrip('\n')
             if msg == 'flash':
-                STATUS_LED.value(1)
-                utime.sleep(0.5)
-                STATUS_LED.value(0)
+                # STATUS_LED.value(1)
+                for i in range(10):  # DEBUG
+                    DEBUG_LED.value(1)
+                    utime.sleep(0.1)
+                    DEBUG_LED.value(0)
+                    utime.sleep(0.1)
+                # STATUS_LED.value(0)
                 sys.stdout.write("done")
-            elif msg == 'calibmag':
+            elif msg == 'calibmag':  # FIXME
                 with open('magno_offset.txt', 'w') as f:
                     f.write(str(' '.join(map(str, magno.calibrate()))))
                 sensor = mpu9250.MPU9250(i2c, ak8963=magno, mpu6500=mpu6500)
@@ -77,11 +89,13 @@ while True:
                 utime.sleep(0.5)
                 STATUS_LED.value(0)
                 session_files = []
-                if os.path.exists("./sessions"):
-                    for fname in os.listdir('./sessions'):
-                        # if os.path.getsize() doesn't work, use os.stat().st_size
-                        if os.path.getsize(fname):
-                            session_files.append(fname)
+                try:
+                    os.stat("./sessions")
+                except OSError:
+                    os.mkdir("./sessions")
+                for fname in os.listdir('./sessions'):
+                    if os.stat(f'./sessions/{fname}')[6]:
+                        session_files.append(fname)
                     # try:
                     #     with open(fname, 'r') as f:
                     #         data = f.read()
@@ -95,13 +109,16 @@ while True:
                     #         sys.stdout.write(err)
                     #     except NameError:
                     #         print("'data' was not defined")
-                    sys.stdout.write(str(len(session_files))+'\n')
-                    for fname in session_files:
-                        with open(fname, 'r') as f:
-                            data = f.read()
-                            sys.stdout.write(fname[:-4]+'\n')      # session timestamp
-                            sys.stdout.write(str(len(data))+'\n')  # number of bytes in data
-                            sys.stdout.write(data)                 # session data
+                sys.stdout.write(str(len(session_files))+'\n')
+                for fname in session_files:
+                    with open(f'./sessions/{fname}', 'r') as f:
+                        data = f.read()
+                        sys.stdout.write(fname[:-4]+'\n')      # session timestamp
+                        sys.stdout.write(str(len(data))+'\n')  # number of bytes in data
+                        sys.stdout.write(data)                 # session data
+        else:
+            # print('no data')
+            utime.sleep(1)
 
     STATUS_LED.value(1)
 
@@ -112,10 +129,10 @@ while True:
 
     STATUS_LED.value(0)
 
-    with open(f'./sessions/{int(utime.time())}.bin', 'wb') as logfile:
+    with open(f'./sessions/{utime.time()}.bin', 'wb') as logfile:
         while START_STOP_BUTTON.value() != 1:
             # 4 bytes per number
-            logfile.write(ustruct.pack('f'*9, sensor.acceleration+sensor.gyro+sensor.magnetic))
+            logfile.write(ustruct.pack('f'*9, *(sensor.gyro+sensor.acceleration+(sensor.magnetic[1], sensor.magnetic[0], -sensor.magnetic[2]))))
             utime.sleep(1/SAMPLE_FREQ)
 
     # NOTE: Consider changing hang time
