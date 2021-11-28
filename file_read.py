0e9b2dcee38fb526b041bc3efda52ab85612d5de
PrajwalVandana <72575914+PrajwalVandana@users.noreply.github.com>
2021-11-27 16:58:10 -0800

    switched to base-64 encoding of floats

--- file_read.py
+++
@@ -1,8 +0,0 @@
-import os
-import ustruct
-
-for f in os.listdir():
-    if f.endswith('.bin'):
-        with open(f, 'rb') as logfile:
-            data = b''.join(logfile.readlines())
-            print(ustruct.unpack('f'*(len(data)//32), data))
--- main.py
+++ main.py
@@ -16,18 +16,32 @@ PRAJWAL = True
 
 SDA = machine.Pin(0)
 SCL = machine.Pin(1)
-START_STOP_BUTTON = machine.Pin(19)
-# CALIB_BUTTON = machine.Pin(14)
-STATUS_LED = machine.Pin(20, machine.Pin.OUT)
-STATUS_LED.value(1)  # test
+START_STOP_BUTTON = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
+STATUS_LED = machine.Pin(15, machine.Pin.OUT)
 DEBUG_LED = machine.Pin(25, machine.Pin.OUT)
+STATUS_LED.value(1)  # test
 utime.sleep(1)  # test
 STATUS_LED.value(0)
+BUTTON_HANG = 0.5
 # ----------------------------- #
+
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 
 
 def get_size(filename):
     return os.stat(filename)[6]
+
+
+def to_base64(num):
+    lst = []
+    while num:
+        lst.append(num % 64)
+        num = num//64
+    res = ''
+    for n in lst[::-1]:
+        res += alphabet[n]
+
+    return res
 
 
 PRAJWAL_MAGNO_OFFSET = (29.04609, 34.06641, -52.03125)
@@ -59,30 +73,28 @@ except OSError:
 
 sensor = mpu9250.MPU9250(i2c, ak8963=magno, mpu6500=mpu6500)
 
-# serialport = machine.UART(1, 115200)
-# serialport.readline()
 
-# sys.stdout.write('test\n') # DEBUG
 print(START_STOP_BUTTON.value())  # DEBUG
 while True:
     # not logging, checking if connected to app
     while START_STOP_BUTTON.value() != 1:
         DEBUG_LED.value(1)
-        utime.sleep(0.5)
+        utime.sleep(0.1)
         DEBUG_LED.value(0)
+        utime.sleep(0.1)
         # NOTE: inefficient, consider porting to select.poll()
         if select.select([sys.stdin], [], [], 0)[0]:
             msg = sys.stdin.readline().rstrip('\n')
             if msg == 'flash':
-                # STATUS_LED.value(1)
                 for i in range(10):  # DEBUG
                     DEBUG_LED.value(1)
+                    # STATUS_LED.value(1)
                     utime.sleep(0.1)
                     DEBUG_LED.value(0)
+                    # STATUS_LED.value(0)
                     utime.sleep(0.1)
-                # STATUS_LED.value(0)
                 sys.stdout.write("done")
-            elif msg == 'calibmag':  # FIXME
+            elif msg == 'calibmag':
                 with open('magno_offset.txt', 'w') as f:
                     f.write(str(' '.join(map(str, magno.calibrate()))))
                 sensor = mpu9250.MPU9250(i2c, ak8963=magno, mpu6500=mpu6500)
@@ -101,6 +113,8 @@ while True:
                 for fname in os.listdir('./sessions'):
                     if get_size(f'./sessions/{fname}'):
                         session_files.append(fname)
+                    else:
+                        os.remove(f'./sessions/{fname}')
                     # try:
                     #     with open(fname, 'r') as f:
                     #         data = f.read()
@@ -118,28 +132,39 @@ while True:
                 for fname in session_files:
                     with open(f'./sessions/{fname}', 'r') as f:
                         data = f.read()
-                        sys.stdout.write(fname[:-4]+'\n')      # session timestamp
-                        sys.stdout.write(str(get_size(f'./sessions/{fname}'))+'\n')  # number of bytes in data
-                        sys.stdout.write(data)                 # session data
-        else:
-            # print('no data')
-            utime.sleep(1)
+                        sys.stdout.write(fname[:-4]+'\n')  # session timestamp
+                        sys.stdout.write(data+'\n')  # session data
+                    # os.remove(f'./sessions/{fname}')
 
     STATUS_LED.value(1)
+    print('waiting for calib')  # DEBUG
+
+    utime.sleep(BUTTON_HANG)
 
     while START_STOP_BUTTON.value() != 1:
         continue
 
     mpu6500.calibrate()
+    print('calibrated')
 
     STATUS_LED.value(0)
 
-    with open(f'./sessions/{utime.time()}.bin', 'wb') as logfile:
+    try:
+        os.stat("./sessions")
+    except OSError:
+        os.mkdir("./sessions")
+
+    max_file_num = 0
+    for fname in os.listdir('./sessions'):
+        if fname.endswith('.txt'):
+            max_file_num = max(max_file_num, int(fname[:-4]))
+
+    with open(f'./sessions/{max_file_num+1}.txt', 'w') as logfile:
         while START_STOP_BUTTON.value() != 1:
-            # 4 bytes per number
-            logfile.write(ustruct.pack('f'*9, *(sensor.gyro+sensor.acceleration
-                                                + (sensor.magnetic[1], sensor.magnetic[0], -sensor.magnetic[2]))))
+            # 4 bytes per number (little-endian) that are converted to base-64
+            logfile.write(to_base64(int.from_bytes(ustruct.pack('<'+'f'*9, *(sensor.gyro+sensor.acceleration
+                                                                             + (sensor.magnetic[1], sensor.magnetic[0], -sensor.magnetic[2]))), 'little')))
             utime.sleep(1/SAMPLE_FREQ)
 
     # NOTE: Consider changing hang time
-    utime.sleep(0.5)  # so that one button press is not counted as multiple
+    utime.sleep(BUTTON_HANG)  # so that one button press is not counted as multiple
